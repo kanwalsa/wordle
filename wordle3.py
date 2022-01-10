fd22e7370ef454add5f08c6f373ccdc86e6e99d9
Kanwal <97309939+kanwalsa@users.noreply.github.com>
2022-01-09 21:02:07 -0500

    find word with heaviest weight

---
+++ wordle3.py
@@ -0,0 +1,46 @@
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+import os
+try :
+    dir = '/Users/kanwal/Desktop/Mason/'
+    path = dir + 'fiveletterwords.txt'
+    print(path)
+    f = open(path, 'r')
+
+   #if this isn't working, you need to change directory in terminal to
+    # where this file is stored.
+
+    worddict = {}
+    lettercount = {}
+    wordcount = 0
+
+    for line in f :
+        worddict[line.strip()] = 0
+        for letter in line:
+            if letter in list(lettercount.keys()):
+                lettercount[letter] += 1 
+            else:
+                lettercount[letter] = 1 
+        wordcount += 1
+
+except FileNotFoundError: 
+    print("Word file not found. Please make sure it's in the same directory as this python file.")
+except Exception as e :
+    print(e)
+
+
+letterlist = list(lettercount.keys())
+lettersum = sum(list(lettercount.values()))
+
+for letter in letterlist:
+    lettercount[letter] = lettercount[letter]/lettersum
+print('words analyzed:', wordcount)
+
+# now move on to word analysis
+
+for word in list(worddict.keys()) :
+    for letter in word:
+        worddict[word] = worddict[word] + lettercount[letter]
+
+keymax = max(worddict, key= lambda x: worddict[x])
+print(keymax, ':',  worddict[keymax])
